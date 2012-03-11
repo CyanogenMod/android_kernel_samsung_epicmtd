5c308c4f2742cc028b12c593fbd0c8da77879204
Brian Beloshapka <bbelos@hotmail.com>
2012-03-10 23:34:12 -0600

    VICTORY: Addition to gp2a.h

--- include/linux/gp2a.h
+++ include/linux/gp2a.h
@@ -26,6 +26,7 @@
 #define GP2A_OPT "gp2a-opt"
 struct gp2a_platform_data {
 	int p_out;  /* proximity-sensor-output gpio */
+	int p_irq;
 	int (*power)(bool); /* power to the chip */
 	int (*light_adc_value)(void); /* get light level from adc */
 	int light_adc_max;
